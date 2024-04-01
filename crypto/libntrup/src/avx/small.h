aabf104e67f3a62154e6b45939feaf2bd482ad65
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-01 18:02:40 +0530

    crypto: libntrup[avx(small)]

---
+++ crypto/libntrup/src/avx/small.h
@@ -0,0 +1,27 @@
+/**
+ * @file small.h
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2024-04-01
+ *
+ * @copyright Copyright (c) 2024 Krisna Pranav, NowWallet Authors
+ *
+ */
+
+#ifndef small_h
+#define small_h
+
+#include <sodium/crypto_int8.h>
+
+typedef crypto_int8 small;
+
+#define small_encode crypto_kem_sntrup4591761_avx_small_encode
+extern void small_encode(unsigned char *, const small *);
+
+#define small_decode crypto_kem_sntrup4591761_avx_small_decode
+extern void small_decode(small *, const unsigned char *);
+
+#define small_random crypto_kem_sntrup4591761_avx_small_random
+extern void small_random(small *);
+
+#endif
