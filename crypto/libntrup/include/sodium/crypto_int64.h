693be3eb3691cf9390da9f972293e8a35ea431ad
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-01 11:22:36 +0530

    crypto: sodium[crypto_int64]

---
+++ crypto/libntrup/include/sodium/crypto_int64.h
@@ -0,0 +1,14 @@
+/**
+ * @file crypto_int64.h
+ * @author Krisna Pranav
+ * @brief crypto_int64
+ * @version 1.0
+ * @date 2024-03-30
+ *
+ * @copyright Copyright (c) 2024 Krisna Pranav, NowWallet Authors
+ *
+ */
+
+#include <stdint.h>
+
+typedef int64_t crypto_int64;
