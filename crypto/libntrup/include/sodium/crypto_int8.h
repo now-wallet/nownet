ef0c80c0854712e1461f52a00fb27f60c2e64169
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-01 11:33:12 +0530

    crypto: sodium[crypto_int8]

---
+++ crypto/libntrup/include/sodium/crypto_int8.h
@@ -0,0 +1,14 @@
+/**
+ * @file crypto_int8.h
+ * @author Krisna Pranav
+ * @brief crypto_int8
+ * @version 1.0
+ * @date 2024-03-30
+ *
+ * @copyright Copyright (c) 2024 Krisna Pranav, NowWallet Authors
+ *
+ */
+
+#include <stdint.h>
+
+typedef int8_t crypto_int8;
