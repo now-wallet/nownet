c17f27fd006ed2372f949e91c4cfec752f519875
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-01 10:35:20 +0530

    include: larp

--- include/larp.hpp
+++ include/larp.hpp
@@ -40,6 +40,77 @@ namespace larp
         class ThreadPool;
     } // namespace thread
 
+    struct RuntimeOptions
+    {
+        bool showBanner = true;
+        bool debug = false;
+        bool isNode = false;
+    }; // struct RuntimeOptions
+
+    struct Context
+    {
+        std::shared_ptr<Crypto> crypto = nullptr;
+        std::shared_ptr<CryptoManager> cryptoManager = nullptr;
+        std::shared_ptr<AbstractRouter> router = nullptr;
+        std::shared_ptr<EventLoop> loop = nullptr;
+        std::shared_ptr<NodeDB> nodedb = nullptr;
+
+        Context() virtual ~Context() = default;
+
+        /**
+         * @param opts
+         */
+        void Setup(const RuntimeOptions &opts);
+
+        /**
+         * @param opts
+         * @return int
+         */
+        int Run(const RuntimeOptions &opts);
+
+        /**
+         * @param sig
+         */
+        void HandleSignal(int sig);
+
+        /**
+         * @param conf
+         */
+        void Configure(std::shared_ptr<Config> conf);
+
+        void Reload();
+
+        bool IsUp() const;
+
+        bool LooksAlive() const;
+
+        bool IsStopping() const;
+
+        void CloseAsync();
+
+        void Wait();
+
+        bool CallSafe(std::function<void(void)> f);
+
+        virtual std::shared_ptr<AbstractRouter> makeRouter(const std::shared_ptr<EventLoop> &loop);
+
+        virtual std::shared_ptr<NodeDB> makeNodeDB();
+
+        virtual std::shared_ptr<larp::vpn::Platform> makeVPNPlatform();
+
+        int androidFD = -1;
+
+    protected:
+        std::shared_ptr<Config> config = nullptr;
+
+    private:
+        void SigINT();
+
+        void Close();
+
+        std::unique_ptr<std::promise<void>> closeWaiter;
+
+    }; // struct Context
 } // namespace larp
 
 #endif
