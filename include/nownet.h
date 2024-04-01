9eb602a5effd65b9c786dcf35f90dc33d6ef18ac
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-01 10:42:24 +0530

    include: nownet[entry point]

---
+++ include/nownet.h
@@ -0,0 +1,19 @@
+/**
+ * @file nownet.h
+ * @author Krisna Pranav
+ * @brief nownet starting point
+ * @version 1.0
+ * @date 2024-04-01
+ *
+ * @copyright Copyright (c) 2024 Krisna Pranav, NowWallet Authors
+ *
+ */
+
+#pragma once
+
+#include "nownet/context.h"
+#include "nownet/srv.h"
+#include "nownet/misc.h"
+#include "nownet/addr.h"
+#include "nownet/stream.h"
+#include "nownet/udp.h"
