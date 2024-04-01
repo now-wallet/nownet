98118b523a0c0956d5098d1d251a1d317eb640c8
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-01 11:20:02 +0530

    crypto: sodium[crypto_int16]

---
+++ crypto/libntrup/include/sodium/crypto_int16.h
@@ -0,0 +1,14 @@
+/**
+ * @file crypto_int16.h
+ * @author Krisna Pranav
+ * @brief crypto_int16
+ * @version 1.0
+ * @date 2024-03-30
+ *
+ * @copyright Copyright (c) 2024 Krisna Pranav, NowWallet Authors
+ *
+ */
+
+#include <stdint.h>
+
+typedef int16_t crypto_int16;
