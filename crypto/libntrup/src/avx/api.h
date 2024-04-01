79e0cd7d3e639468aad8e75b5430680edbe3ac81
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-01 18:00:14 +0530

    crypto: libntrup[api]

---
+++ crypto/libntrup/src/avx/api.h
@@ -0,0 +1,15 @@
+/**
+ * @file api.h
+ * @author Krisna Pranav
+ * @brief api
+ * @version 1.0
+ * @date 2024-04-01
+ *
+ * @copyright Copyright (c) 2024 Krisna Pranav, NowWallet Authors
+ *
+ */
+
+#define CRYPTO_SECRETKEYBYTES 1600
+#define CRYPTO_PUBLICKEYBYTES 1218
+#define CRYPTO_CIPHERTEXTBYTES 1047
+#define CRYPTO_BYTES 32
