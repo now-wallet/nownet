3c768d812ded1f4a5f67db9b1ae212ee4e068b5b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-01 18:06:07 +0530

    crypto: libntrup[avx(params)]

---
+++ crypto/libntrup/src/avx/params.h
@@ -0,0 +1,24 @@
+/**
+ * @file params.h
+ * @author Krisna Pranav
+ * @brief params
+ * @version 1.0
+ * @date 2024-04-01
+ *
+ * @copyright Copyright (c) 2024 Krisna Pranav, NowWallet Authors
+ *
+ */
+
+#ifndef params_h
+#define params_h
+
+#define q 4591
+
+#define qshift 2295
+#define p 761
+#define w 286
+
+#define rq_encode_len 1218
+#define small_encode_len 191
+
+#endif
