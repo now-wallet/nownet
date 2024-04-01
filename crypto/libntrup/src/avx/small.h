03a22945b2c1f901cd6b79f76cf2b4030b040a4c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-01 18:03:56 +0530

    crypto: libntrup[avx(small)]

--- crypto/libntrup/src/avx/small.h
+++ crypto/libntrup/src/avx/small.h
@@ -24,4 +24,7 @@ extern void small_decode(small *, const unsigned char *);
 #define small_random crypto_kem_sntrup4591761_avx_small_random
 extern void small_random(small *);
 
+#define small_random_weightw crypto_kem_sntrup4591761_avx_small_random_weightw
+extern void small_random_weightw(small *);
+
 #endif
