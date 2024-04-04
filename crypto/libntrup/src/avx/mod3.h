429327c21c5a4782be6d62417f8e71f23e91fff7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-04 13:11:50 +0530

    crypto: libntrup[avx(mod3)]

--- crypto/libntrup/src/avx/mod3.h
+++ crypto/libntrup/src/avx/mod3.h
@@ -35,4 +35,73 @@ static inline small mod3_freeze(crypto_int32 a)
     return a;
 }
 
+/**
+ * @param a
+ * @param b
+ * @param c
+ * @return small
+ */
+static inline small mod3_minusproduct(small a, small b, small c)
+{
+    crypto_int32 A = a;
+    crypto_int32 B = b;
+    crypto_int32 C = c;
+    return mod3_freeze(A - B * C);
+}
+
+/**
+ * @param a
+ * @param b
+ * @param c
+ * @return small
+ */
+static inline small mod3_plusproduct(small a, small b, small c)
+{
+    crypto_int32 A = a;
+    crypto_int32 B = b;
+    crypto_int32 C = c;
+    return mod3_freeze(A + B * C);
+}
+
+/**
+ * @param a
+ * @param b
+ * @return small
+ */
+static inline small mod3_product(small a, small b)
+{
+    return a * b;
+}
+
+/**
+ * @param a
+ * @param b
+ * @return small
+ */
+static inline small mod3_sum(small a, small b)
+{
+    crypto_int32 A = a;
+    crypto_int32 B = b;
+    return mod3_freeze(A + B);
+}
+
+/**
+ * @param a1
+ * @return small
+ */
+static inline small mod3_reciprocal(small a1)
+{
+    return a1;
+}
+
+/**
+ * @param num
+ * @param den
+ * @return small
+ */
+static inline small mod3_quotient(small num, small den)
+{
+    return mod3_product(num, mod3_reciprocal(den));
+}
+
 #endif
