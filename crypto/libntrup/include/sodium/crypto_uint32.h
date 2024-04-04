4b12c05e92b574948752daa80d07f6530f7d6e1e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-04 12:58:37 +0530

    crypto: libntrup[crypto_uint32]

---
+++ crypto/libntrup/include/sodium/crypto_uint32.h
@@ -0,0 +1,16 @@
+/**
+ * @file crypto_uint32.h
+ * @author Krisna Pranav
+ * @brief Crypto uint32
+ * @version 1.0
+ * @date 2024-04-04
+ *
+ * @copyright Copyright (c) 2024 Krisna Pranav, NowWallet Authors
+ *
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+typedef uint32_t crypto_uint32;
