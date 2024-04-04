8da740fd2fceab6ee37d43d2fd17e70c48efbfb0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-04 16:44:13 +0530

    crypto: libntrup[avx(r3)]

--- crypto/libntrup/src/avx/r3.h
+++ crypto/libntrup/src/avx/r3.h
@@ -20,4 +20,7 @@ extern void r3_mult(small *, const small *, const small *);
 #define r3_recip crypto_kem_sntrup4591761_avx_r3_recip
 extern int r3_recip(small *, const small *);
 
+#define r3_weightw_mask crypto_kem_sntrup4591761_avx_r3_weightw_mask
+extern int r3_weightw_mask(const small *);
+
 #endif
