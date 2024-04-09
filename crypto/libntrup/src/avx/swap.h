8c4e09dc3104f8913a52f845343fc3137c150371
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-09 11:15:17 +0530

    crypto: libntrup[swap]

---
+++ crypto/libntrup/src/avx/swap.h
@@ -0,0 +1,18 @@
+/**
+ * @file swap.h
+ * @author Krisna Pranav
+ * @brief swap
+ * @version 1.0
+ * @date 2024-04-09
+ *
+ * @copyright Copyright (c) 2024 Krisna Pranav, NowWallet Authors
+ *
+ */
+
+#ifndef swap_h
+#define swap_h
+
+#define swap crypto_kem_sntrup4591761_avx_swap
+extern void swap(void *, void *, int, int);
+
+#endif
